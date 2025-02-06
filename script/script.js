e5eee74bcc051123359d4886bd00debe32adc71f
AnthonyM <aenriquemartinez@gmail.com>
2025-02-06 03:51:36 -0500

    fixed responsive mode 320p

--- script/script.js
+++ script/script.js
@@ -12,28 +12,39 @@ let popup__form = document.querySelector(".popup__form");
 
 input_name.placeholder = name_profile.textContent;
 input_about.placeholder = about__profile.textContent;
-console.log(`${name_profile.textContent}`);
+
 function open_popup() {
   popup.classList.add("popup_opened");
 }
-
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   if (input_name.value != "") {
     name_profile.textContent = input_name.value;
+    console.log("prueba");
   }
   if (input_about.value != "") {
     about__profile.textContent = input_about.value;
   }
   popup.classList.remove("popup_opened");
 }
+function checkInputFilled() {
+  if (input_name.value.trim() != "" || input_about.value.trim() != "") {
+    button_save.classList.add("popup__button_save_filled");
+  } else {
+    button_save.classList.remove("popup__button_save_filled");
+  }
+}
 
 button_edit.addEventListener("click", open_popup);
-
-button_add.addEventListener("click", function () {
-  console.log("add button");
-});
+input_name.addEventListener("input", checkInputFilled);
+input_about.addEventListener("input", checkInputFilled);
 button_close.addEventListener("click", function () {
+  input_name.value = "";
+  input_about.value = "";
+  button_save.classList.remove("popup__button_save_filled");
   popup.classList.remove("popup_opened");
 });
 button_save.addEventListener("click", handleProfileFormSubmit);
+button_add.addEventListener("click", function () {
+  console.log("add button");
+});

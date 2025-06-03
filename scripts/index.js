b5a330ba047ebe65cf4c941119dd3e0d821e625f
Anthony Martinez <aenriquemartinez@gmail.com>
2025-06-03 23:33:23 +0200

    added function create card new place

--- scripts/index.js
+++ scripts/index.js
@@ -15,12 +15,16 @@ let dialog__form = document.querySelector("#formEdit");
 let form__addPLace = document.querySelector("#formAddPlace");
 let input_title = document.querySelector("#inputAddTitle");
 let input_link = document.querySelector("#inputAddlink");
+let elementSection = document.querySelector("#elementsSection");
 
 input_name.placeholder = name_profile.textContent;
 input_about.placeholder = about__profile.textContent;
 
 function open_dialog() {
   dialog.classList.add("dialog__opened");
+}
+function closeAddFuntion() {
+  addPLace.classList.remove("dialog__opened");
 }
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
@@ -39,6 +43,34 @@ function checkInputFilled() {
     button_save.classList.remove("dialog__button-save-filled");
   }
 }
+function createCard() {
+  const divCard = document.createElement("div");
+  const imgCard = document.createElement("img");
+  const imgLike = document.createElement("img");
+  const divInfo = document.createElement("div");
+  const cardName = document.createElement("h3");
+  const infoName = document.createTextNode("Hola, mundo");
+  const buttonLike = document.createElement("button");
+
+  divCard.classList.add("card");
+  imgCard.classList.add("card__img");
+  imgLike.classList.add("card__like-logo");
+  divInfo.classList.add("card__container");
+  cardName.classList.add("card__name");
+  buttonLike.classList.add("card__like");
+  imgCard.src = "images/place1.jpg";
+  imgLike.src = "images/like.svg";
+  imgLike.alt = "like logo";
+
+  buttonLike.appendChild(imgLike);
+  cardName.appendChild(infoName);
+  divInfo.appendChild(cardName);
+  divInfo.appendChild(buttonLike);
+  divCard.appendChild(imgCard);
+  divCard.appendChild(divInfo);
+  elementSection.prepend(divCard);
+  closeAddFuntion();
+}
 
 button_edit.addEventListener("click", open_dialog);
 input_name.addEventListener("input", checkInputFilled);
@@ -53,6 +85,5 @@ button_save.addEventListener("click", handleProfileFormSubmit);
 button_add.addEventListener("click", function () {
   addPLace.classList.add("dialog__opened");
 });
-button_closeAdd.addEventListener("click", function () {
-  addPLace.classList.remove("dialog__opened");
-});
+button_closeAdd.addEventListener("click", closeAddFuntion);
+button_save_place.addEventListener("click", createCard);
